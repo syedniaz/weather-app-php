f517ff5ff4217d1f3f50d1a579f82459f6223dd3
syedniaz <snm051110@gmail.com>
2023-11-26 21:05:38 +0600

    linking style.css for vercel

--- api/index.php
+++ api/index.php
@@ -16,7 +16,7 @@ $climate = fetchData($city, "", "");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weather Today!</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
